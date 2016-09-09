b1b71870e9aff3a2c1d89a3c15f5a6a42f65a4a4
unknown <>
2016-09-09 21:31:15 +0800

    'auto_commit'

--- C/COOL/COOL/main.cpp
+++ C/COOL/COOL/main.cpp
@@ -1,25 +1,50 @@
-#include<iostream>
-#include<fstream>
-#include<string>
-#include<vector>
-using namespace std;
+#include "common.h"
+#include "token.h"
 
-vector<string> readFile(char *file){
-	ifstream in(file);
-	vector<string> buf;
-	string s;
-	while (getline(in,s))
-		buf.push_back(s);
-	return buf;
-}
 void lex(char *file){
-	vector<string> codes = readFile(file);
-	for (int i = 0; i < codes.size(); i++){
-		cout << i + 1 << ":" << codes[i] << endl;
+	//文件读入缓冲区
+	FILE* f = fopen(file, "rb");
+	fseek(f, 0, SEEK_END);
+	long lSize = ftell(f);
+	char* buf = (char*)malloc(sizeof(char)*(lSize+1));
+	size_t result;
+	rewind(f);
+	if (buf)result = fread(buf, 1, lSize, f);
+	fclose(f);
+	if (result != lSize)return;
+	buf[lSize] = 0;
+	//printf("%s", buf);
+	//生成token流
+	int pos = 0; char c; int dfa_begin = 0;
+	while (pos <= lSize){
+		c = buf[pos];
+		if (isalpha(c) || c == '_'){
+			pos++;
+			goto ID;
+		}
+		else if (isdigit(c)){
+			pos++;
+			goto INT;
+		}
+		else if (c == '"'){
+			pos++;
+			goto STR;
+		}
+		else if (c == '-'){
+			pos++;
+			goto MNS_CMT;
+		}
+		else if (c == '('){
+			pos++;
+			goto CMT_LPA;
+		}
+	ID:
+	INT:
+	STR:
+			   MNS_CMT:
 	}
 }
+
 int main(int argc, char *argv[]){
-	if (argc == 2){
-		lex(argv[1]);
-	}
+	lex("C:\\C\\C\\C\\COOL\\Debug\\main.cpp");
 }
